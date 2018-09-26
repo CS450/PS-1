dbacb01a741d4fcff5eaa5e3e6e4c45a3014d048
sppenna <leveetoo@gmail.com>
2018-09-25 17:48:03 -0700

    changed Makefile, added p2.c, p4.cpp, p4.txt

--- Makefile
+++ Makefile
@@ -2,10 +2,14 @@ all:p1.x p3.x p3assembly.s
 
 p1.x: p1.c
 	gcc p1.c -o p1.x
+p2.x: p2.c
+	gcc p2.c -o p2.x
 p3.x: p3.cpp
 	g++ -std=c++11 p3.cpp -o p3.x
 p3assembly.s: p3.cpp
 	g++ -std=c++11 -S -o p3.s p3.cpp
+p4.x: p4.cpp
+	g++ -std=c++11 p4.cpp -o p4.x
 
 clean: 
 	rm -f *.o *~ *.x p3assembly.s p3.s
---
+++ p2.c
@@ -0,0 +1,38 @@
+/*
+CS450-Problemset 1 #2:
+What: This program sets an alarm and exits after 3 seconds.
+Why: The signal is called in main using SIGALRM, and alarm_handler as its types.
+the alarm_handler is set to = 1 in the alarm_handler function.  Then alarm() is called with the value
+3 so that the alarm is signalled after 3 seconds.
+*/
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* The signal handler isn't allowed to do anything except
+    * access this variable and re-invoke signal().
+     */
+volatile sig_atomic_t alarm_flag = 0;
+
+/* Prototype for signal handler */
+void alarm_handler (int sig);
+
+int main (void) {
+  // Call signal() to set up a handler for SIGALRM
+    signal(SIGALRM, alarm_handler);
+
+  // Call alarm() to have the alarm go off in a few seconds
+    alarm(3);
+
+    while (!alarm_flag) {
+        puts("Waiting for an alarm");
+    }
+    printf("done\n");
+    return 0;
+}
+
+void alarm_handler(int sig) {
+    alarm_flag = 1;
+}
+
